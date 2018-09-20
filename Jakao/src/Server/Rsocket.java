3f94f99034267b757ed651d0507db06bb1508ec6
La <35015234+Las-Wonho@users.noreply.github.com>
2018-09-20 20:19:15 +0900

    소켓 편하게 쓰기 위해 만듬

---
+++ Jakao/src/Server/Rsocket.java
@@ -0,0 +1,43 @@
+package Server;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+public class Rsocket {
+public Socket socket;
+private BufferedReader br;
+private OutputStream os;
+
+public Rsocket(Socket s){
+	socket = s;
+	try {
+		br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		os = socket.getOutputStream();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+
+public void send(String str) {
+	try {
+		os.write(str.getBytes());
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		System.out.println("Send error");
+	}
+}
+
+public String recive() {
+	try {
+		return br.readLine();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return "";
+}
+}
